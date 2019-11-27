cd1da3b712e4e96da208e6d402094e490eb6c048
Linus Norton <linusnorton@gmail.com>
2019-11-27 07:49:51 +0000

    Added pm2 ecosystem to travis deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    apps : [{
+        name: "eco-rewards-hub",
+        script: "ts-node",
+        args: "src/start.ts",
+        env: {
+            NODE_ENV: "development",
+        },
+        env_production: {
+            NODE_ENV: "production",
+        }
+    }],
+    deploy : {
+        production : {
+            "ssh_options": "StrictHostKeyChecking=no",
+            "key": "deploy",
+            "user": "ubuntu",
+            "host": ["35.178.27.220"],
+            "ref": "origin/master",
+            "repo": "git@github.com:EcoRewards/eco-rewards-hub.git",
+            "path": "/home/ubuntu/eco-rewards-hub",
+            "post-deploy": "npm install; npm run migrate",
+            "env"  : {
+                "NODE_ENV": "production"
+            }
+        }
+    }
+};
