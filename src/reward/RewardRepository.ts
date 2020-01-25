a27e0f7451bb3eec262af68ff6505d11284841c7
Linus Norton <linusnorton@gmail.com>
2020-01-25 21:33:33 +0000

    fix typo and add better logging

--- src/reward/RewardRepository.ts
+++ src/reward/RewardRepository.ts
@@ -3,6 +3,7 @@ import { Journey } from "../journey/Journey";
 import { Connection } from "mysql";
 import { NonNullId } from "..";
 import { pushNested } from "ts-array-utils";
+import { Logger } from "pino";
 
 /**
  * Database queries specific to reward allocation. These span the journey and member tables.
@@ -10,7 +11,8 @@ import { pushNested } from "ts-array-utils";
 export class RewardRepository {
 
   constructor(
-    private readonly db: any
+    private readonly db: any,
+    private readonly logger: Logger
   ) { }
 
   /**
@@ -42,7 +44,7 @@ export class RewardRepository {
     journeysProcessed: any[],
     rewardsGenerated: number,
     carbonSavingGenerated: number,
-    totalDistance: number
+    totalMiles: number
   ): Promise<void> {
     const connection = await this.db.getConnection();
     await connection.beginTransaction();
@@ -52,9 +54,9 @@ export class RewardRepository {
         `UPDATE member SET 
            rewards = rewards + ?, 
            carbon_saving = carbon_saving + ?,
-           total_distance = total_distance + ?,
+           total_miles = total_miles + ?
          WHERE id = ?`,
-        [rewardsGenerated, carbonSavingGenerated, totalDistance, memberId]
+        [rewardsGenerated, carbonSavingGenerated, totalMiles, memberId]
       );
 
       const journeyUpdates = journeysProcessed.map(journey => {
@@ -67,6 +69,9 @@ export class RewardRepository {
       await connection.commit();
     }
     catch (err) {
+      this.logger.warn("Error processing journeys, rolling back transaction");
+      this.logger.warn(err);
+
       await connection.rollback();
     }
     finally {
