4435833e989a73f7df4f326cae2f46afdfdb2bf6
Linus Norton <linusnorton@gmail.com>
2021-01-26 19:30:44 +0000

    fix tap processing

--- src/journey/TapReader.spec.ts
+++ src/journey/TapReader.spec.ts
@@ -20,6 +20,16 @@ describe("TapReader", () => {
     chai.expect(journeys["6338000085351501"]).to.equal("2019-12-19T14:56");
   });
 
+  it("ignores type 00 transactions", () => {
+    const hex = "03 40 79 35 00 00 12 46 00 00 00 00 00 00 00 00 00 10 67 3a 4c 63 38 00 00 15 53 74 02 10 67 3a";
+    const noSpace = hex.split(" ").join("");
+    const buffer = Buffer.from(noSpace, "hex");
+    const journeys = reader.getTaps(buffer);
+
+    chai.expect(logger.e).to.equal("");
+    chai.expect(journeys["6338000015537402"]).to.equal("2021-01-16T12:42");
+  });
+
   it("reads an ITSO card and nfc payloads", () => {
     const hex = "051118700000001263003800008535150107BF8054222223001307BF8054222223001207BF81";
     const buffer = Buffer.from(hex, "hex");
@@ -31,7 +41,7 @@ describe("TapReader", () => {
   it("generates a warning for unknown payloads", () => {
     const hex = "0511187000000012AA003800008535150107BF8054222223001307BF8054222223001207BF81";
     const buffer = Buffer.from(hex, "hex");
-    const journeys = reader.getTaps(buffer);
+    reader.getTaps(buffer);
 
     chai.expect(logger.e).to.deep.equal("Unknown card type: aa full data: 05 11 18 70 00 00 00 12 aa 00 38 00 00 85 35 15 01 07 bf 80 54 22 22 23 00 13 07 bf 80 54 22 22 23 00 12 07 bf 81");
   });
--- src/journey/TapReader.ts
+++ src/journey/TapReader.ts
@@ -17,6 +17,11 @@ export class TapReader {
       startOffset: 1,
       length: 8
     },
+    // null transaction
+    0x00: {
+      startOffset: 0,
+      length: 8
+    },
     // NFC
     0x54: {
       startOffset: 1,
@@ -38,11 +43,14 @@ export class TapReader {
       const format = TapReader.FORMATS[data[i]];
 
       if (format) {
-        const card = Array.from(data.slice(i + format.startOffset, i + format.length + 1));
-        const member = card.map(toHex).join("");
-        const minsSinceEpoch = data.readUIntBE(i + format.length + 1, 3);
+        if (data[i] !== 0x00) {
+          const card = Array.from(data.slice(i + format.startOffset, i + format.length + 1));
+          const member = card.map(toHex).join("");
+          const minsSinceEpoch = data.readUIntBE(i + format.length + 1, 3);
 
-        journeys[member] = TapReader.EPOCH.plusMinutes(minsSinceEpoch).toJSON();
+          journeys[member] = TapReader.EPOCH.plusMinutes(minsSinceEpoch).toJSON();
+        }
+
         i = i + format.length + 4;
       }
       else {
