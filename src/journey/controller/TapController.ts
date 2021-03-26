8b186a996199470820098e040cc6eb4a4e791b82
Linus Norton <linusnorton@gmail.com>
2021-03-26 09:38:58 +0000

    log headers

--- src/journey/controller/TapController.ts
+++ src/journey/controller/TapController.ts
@@ -42,6 +42,7 @@ export class TapController {
     this.logger.info("Tap: " + rawData);
 
     if (buffer[8] === 0x20) {
+      console.log(ctx.header);
       const header = ctx.header["X-Downlink-Apikey"] && { Authorization: ctx.header["X-Downlink-Apikey"] };
 
       await this.processStatus(formattedRequest, rawData, header);
