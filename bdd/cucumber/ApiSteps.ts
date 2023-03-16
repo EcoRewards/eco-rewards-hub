6c4aa4f0fe8d94172311bb173b88089a320c4eb7
Linus Norton <linusnorton@gmail.com>
2023-03-16 11:32:23 +0000

    remove logging

--- bdd/cucumber/ApiSteps.ts
+++ bdd/cucumber/ApiSteps.ts
@@ -16,9 +16,4 @@ Given("I am logged in as an administrator", async function() {
     headers: { authorization: "Basic " + result.data.data.token },
   });
 
-  World.api.interceptors.response.use((response) => response, (error) => {
-    console.error(error);
-
-    throw error;
-  });
 });
