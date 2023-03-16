92af0753b91fb9771a874c07b108f339345ff2eb
Linus Norton <linusnorton@gmail.com>
2023-03-16 10:56:33 +0000

    debug

--- bdd/cucumber/ApiSteps.ts
+++ bdd/cucumber/ApiSteps.ts
@@ -15,4 +15,10 @@ Given("I am logged in as an administrator", async function() {
     baseURL: host,
     headers: { authorization: "Basic " + result.data.data.token },
   });
+
+  World.api.interceptors.response.use((response) => response, (error) => {
+    console.error(error);
+
+    throw error;
+  });
 });
